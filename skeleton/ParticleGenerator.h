767f587cb37912b1c2d31e6e4709f6c117eec267
Sebana02 <sebana@ucm.es>
2022-10-10 14:23:49 +0200

    gaussian gen

--- skeleton/ParticleGenerator.h
+++ skeleton/ParticleGenerator.h
@@ -5,6 +5,7 @@
 #include <list>
 #include "core.hpp"
 #include "Particle.h"
+#include <random>
 
 using namespace std;
 
@@ -25,7 +26,15 @@ protected:
 
 class GaussianParticleGenerator : public ParticleGenerator {
 public:
+	GaussianParticleGenerator(string name, Vector3 mean_pos, Vector3 mean_vel,Vector3 std_dev_pos, Vector3 std_dev_vel);
 
-};
+	virtual std::list<Particle*> generateParticles();
+
+protected:
+	Vector3 std_dev_pos, std_dev_vel;
+	double std_dev_t;
+
+	std::normal_distribution<double> d{ 0,1 };
+}; 
 #endif __PARTICLE__GENERATOR__
 
